b6ccbde553d3c5f8412ba200e98e81b18c494ad2
Ephylyy <Sarah.Raible@gmx.de>
2022-06-22 14:42:26 +0200

    Changes in ConsoleInputAAS language changed to english

--- src/UILayer/ConsoleInputAAS.java
+++ src/UILayer/ConsoleInputAAS.java
@@ -3,30 +3,30 @@ package UILayer;
 import java.util.Scanner;
 
 public class ConsoleInputAAS {
-    static String eingeben (String eingabeAufforderung) {
-        System.out.println(eingabeAufforderung);
+    static String insert(String userInput) {
+        System.out.println(userInput);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
-    static String waehleAus(String[] alternativen) {
+    static String choose(String[] alternatives) {
         String output = new String();
-        boolean eingabeErfolgreich = false;
-        while (!eingabeErfolgreich) {
-            System.out.println("Bitte geben Sie die gewünschte Nummer ein: ");
-            for (int i = 0; i < alternativen.length; i++) {
-                System.out.println((i + 1) + ") " + alternativen[i]);
+        boolean insertRequired = false;
+        while (!insertRequired) {
+            System.out.println("Please insert number: ");
+            for (int i = 0; i < alternatives.length; i++) {
+                System.out.println((i + 1) + ") " + alternatives[i]);
             }
-            String input = eingeben("Eingabe: ");
+            String input = insert("Input: ");
             if (input.equals("")) {
-                System.out.println("kein Index eingegeben");
-                System.out.println("bitte Eingabe Wiederholen");
-            } else if (Integer.parseInt(input) < 1 || alternativen.length < Integer.parseInt(input)) {
-                System.out.println("Ungültigen Index Eingegeben");
-                System.out.println("bitte Eingabe Wiederholen");
+                System.out.println("No Index entered");
+                System.out.println("Please repeat insert");
+            } else if (Integer.parseInt(input) < 1 || alternatives.length < Integer.parseInt(input)) {
+                System.out.println("Invalid Index entered");
+                System.out.println("Please repeat insert");
             } else {
-                eingabeErfolgreich = true;
-                output = alternativen[Integer.parseInt(input) - 1];
+                insertRequired = true;
+                output = alternatives[Integer.parseInt(input) - 1];
             }
         }
         return output;
